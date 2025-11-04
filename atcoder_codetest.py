6b6fd7a86d0a1cd9bf95d60ef860513cc38b48bc
izuchi <ryousuke201102@gmail.com>
2025-11-04 15:29:03 +0900

    出力処理クラスを追加

--- atcoder_codetest.py
+++ atcoder_codetest.py
@@ -16,7 +16,7 @@ AtCoderInputクラス
 - grid_input: グリッド入力
 
 パラメータ:
-- input_type: 出力の型（int, float, strなど）
+- input_type: 入力の型（int, float, strなど）
 - sep: 区切り文字（デフォルトはスペース）
 - rows: グリッドの行数（grid_input用）
 
@@ -32,21 +32,60 @@ class AtCoderInput: # 入力処理クラス
     def __init__(self):
         pass
 
-    def single_input(self, input_type): # 単一入力
+    def single_input(self, input_type:type): # 単一入力
         return input_type(input())
     
-    def multiple_input(self, input_type, sep=' '): # 複数入力
+    def multiple_input(self, input_type:type, sep=' '): # 複数入力
         return map(input_type, input().split(sep))
     
-    def list_input(self, input_type, sep=' '): # リスト入力
+    def list_input(self, input_type:type, sep=' '): # リスト入力
         return list(map(input_type, input().split(sep)))
     
-    def grid_input(self, rows, input_type, sep=' '): # グリッド入力
+    def grid_input(self, rows:int, input_type:type, sep=' '): # グリッド入力
         grid = []
         for _ in range(rows):
             grid.append(list(map(input_type, input().split(sep))))
         return grid
     
+"""
+AtCoderOutputクラス
+出力処理を簡単にするためのクラス
+
+メソッド:
+- single_output: 単一出力
+- list_output: リスト出力
+- grid_output: グリッド出力
+- print_judge: 判定出力（Yes/No）
+
+パラメータ:
+- value: 出力する単一の値
+- values: 出力するリスト
+- grid: 出力するグリッド
+- sep: 区切り文字（デフォルトはスペース）
+
+使用例:
+aco = AtCoderOutput()
+aco.single_output(42)  # 単一出力
+aco.list_output([1, 2, 3, 4])  # リスト出力
+aco.grid_output([[1, 2], [3, 4]])  # グリッド出力
+aco.print_judge(True)  # 判定出力
+"""
+class AtCoderOutput: # 出力処理クラス
+    def __init__(self):
+        pass
+
+    def single_output(self, value): # 単一出力
+        print(value)
+    
+    def list_output(self, values:list, sep=' '): # リスト出力
+        print(sep.join(map(str, values)))
+
+    def grid_output(self, grid:list, sep=''): # グリッド出力
+        for row in grid:
+            print(sep.join(map(str, row)))
+    
+    def print_judge(self, judge:bool): # 判定出力
+        print("Yes" if judge else "No")
 
 if __name__ == "__main__": # メイン処理
     aci = AtCoderInput()
