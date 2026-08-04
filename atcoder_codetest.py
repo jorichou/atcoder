ab267406511f4c242465098e9ff88b96f7efd6da
izuchi <ryousuke201102@gmail.com>
2026-08-04 20:02:31 +0900

    feat: 累積和関数とセグ木クラスを実装

--- atcoder_codetest.py
+++ atcoder_codetest.py
@@ -33,8 +33,76 @@ def i_list(input_type: type = int, sep=" "):  # リスト入力
 def o_judge(judge: bool):  # 判定出力
     print("Yes" if judge else "No")
 
+def cumulative_sum(l: list[int]): # 累積和を求める
+    ans = [l[0]]
+    for i in range(1, len(l)):
+        ans[i] = ans[i - 1] + l[i]
+    return ans
+
+class SegmentTree:
+    """
+    点更新・区間取得を行う非再帰セグメント木
+    """
+    def __init__(self, size_or_arr: int | list[int], op: function, identity: int):
+        self.op = op
+        self.identity = identity
+
+        if isinstance(size_or_arr, int):
+            self.n = size_or_arr
+            self.tree = [identity] * (2 * self.n)
+        else:
+            self.n = len(size_or_arr)
+            self.tree = [identity] * (2 * self.n)
+            # 初期データのセット
+            for i in range(self.n):
+                self.tree[self.n + i] = size_or_arr[i]
+            # 構築
+            for i in range(self.n - 1, 0, -1):
+                self.tree[i] = self.op(self.tree[2 * i], self.tree[2 * i + 1])
+
+    def update(self, i, val):
+        """位置 i の値を val に更新: O(log N)"""
+        p = i + self.n
+        self.tree[p] = val
+        while p > 1:
+            p >>= 1
+            self.tree[p] = self.op(self.tree[2 * p], self.tree[2 * p + 1])
+
+    def query(self, l, r):
+        """半開区間 [l, r) のクエリ処理: O(log N)"""
+        res_l = self.identity
+        res_r = self.identity
+        l += self.n
+        r += self.n
+
+        while l < r:
+            if l & 1:
+                res_l = self.op(res_l, self.tree[l])
+                l += 1
+            if r & 1:
+                r -= 1
+                res_r = self.op(self.tree[r], res_r)
+            l >>= 1
+            r >>= 1
+
+        return self.op(res_l, res_r)
+
+
 def main():
-    
-    
+    n, m = i_multi()
+    if m == 0:
+        print('No')
+    else:
+        graph = defaultdict(list[int])
+        for _ in range(n):
+            v = sorted(i_list())
+            graph[v[0]].append(v[1])
+
+        if len(graph[1]) != 1 or len(graph[n]) != 1:
+            print("No")
+        else:
+
+
+
 if __name__ == "__main__":  # メイン処理
     main()
